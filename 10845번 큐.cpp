bd59d218d722d8e5c350885c38fbfee6beae2db1
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2020-09-03 20:41:43 +0900

    Add files via upload

---
+++ 10845번 큐.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    vector<int> quene;
+    string tmp;
+    int n;
+    int idx = -1;
+    int tmp2;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> tmp;
+
+        if (tmp == "push")
+        {
+            cin >> tmp2;
+            quene.push_back(tmp2);
+            idx++;
+        }
+        else if (tmp == "pop")
+        {
+            if (idx != -1)
+            {
+                std::cout << quene[0] << "\n";
+                quene.erase(quene.begin());
+                idx--;
+            }
+            else
+            {
+                std::cout << -1 << "\n";
+            }
+        }
+        else if (tmp == "size")
+        {
+            std::cout << idx + 1 << "\n";
+        }
+        else if (tmp == "empty")
+        {
+            if (idx != -1)
+            {
+                std::cout << 0 << "\n";
+            }
+            else
+            {
+                std::cout << 1 << "\n";
+            }
+        }
+        else if (tmp == "front")
+        {
+            if (idx != -1)
+            {
+                std::cout << quene[0] << "\n";
+            }
+            else
+            {
+                std::cout << -1 << "\n";
+            }
+        }
+        else if (tmp == "back")
+        {
+            if (idx != -1)
+            {
+                std::cout << quene[idx] << "\n";
+            }
+            else
+            {
+                std::cout << -1 << "\n";
+            }
+        }
+    }
+}
---
+++ 10866번 덱.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    deque<int> deque;
+    string tmp;
+    int n;
+    int idx = -1;
+    int tmp2;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> tmp;
+
+        if (tmp == "push_front")
+        {
+            cin >> tmp2;
+            deque.push_front(tmp2);
+            idx++;
+        }
+        else if (tmp == "push_back")
+        {
+            cin >> tmp2;
+            deque.push_back(tmp2);
+            idx++;
+        }
+        else if (tmp == "pop_front")
+        {
+            if (idx != -1)
+            {
+                std::cout << deque[0] << "\n";
+                deque.pop_front();
+                idx--;
+            }
+            else
+            {
+                std::cout << -1 << "\n";
+            }
+        }
+        else if (tmp == "pop_back")
+        {
+            if (idx != -1)
+            {
+                std::cout << deque[idx] << "\n";
+                deque.pop_back();
+                idx--;
+            }
+            else
+            {
+                std::cout << -1 << "\n";
+            }
+        }
+        else if (tmp == "size")
+        {
+            std::cout << idx + 1 << "\n";
+        }
+        else if (tmp == "empty")
+        {
+            if (idx != -1)
+            {
+                std::cout << 0 << "\n";
+            }
+            else
+            {
+                std::cout << 1 << "\n";
+            }
+        }
+        else if (tmp == "front")
+        {
+            if (idx != -1)
+            {
+                std::cout << deque[0] << "\n";
+            }
+            else
+            {
+                std::cout << -1 << "\n";
+            }
+        }
+        else if (tmp == "back")
+        {
+            if (idx != -1)
+            {
+                std::cout << deque[idx] << "\n";
+            }
+            else
+            {
+                std::cout << -1 << "\n";
+            }
+        }
+    }
+}
