951b4d8e10554e0946ae687090dc5accecf8fc06
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2020-09-08 23:59:26 +0900

    Add files via upload

---
+++ 11444번 피보나치 수 6.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define mod 1000000007LL
+#define SIZE 1000
+
+int count = 0;
+long long fibo[3];
+struct dp {
+	long long key;
+	long long value;
+};
+
+typedef struct dp dp;
+
+dp memo[SIZE];
+int memoization(long long n);
+long long formula(long long n);
+
+int main(void)
+{
+	long long n;
+	fibo[0] = 0;
+	fibo[1] = 1;
+
+	scanf("%lld", &n);
+
+	for (int i = 0; i < 10 -1; i++)
+	{
+		fibo[i + 2] = (fibo[i] + fibo[i + 1])%mod;
+	}
+
+	printf("%lld", formula(n));
+	return 0;
+}
+long long formula(long long n)
+{
+	if (n <= 10)	//brute force
+	{
+		return fibo[n]%mod;
+	}
+	else if (memoization(n))  // memoization
+	{
+		return memo[memoization(n)].value;
+	}
+	long long tmp, tmp2;
+	
+
+	if (n % 2LL == 1LL)
+	{
+		tmp = formula(((n) / 2LL) + 1LL);
+		tmp2 = formula(((n) / 2LL));
+		memo[count].value=(((tmp * tmp)) + (tmp2 * tmp2)) % mod;
+		memo[count].key = n;
+		count++;
+		return memo[count-1].value;
+	}
+	else
+	{
+		tmp = formula((n / 2LL) - 1LL);
+		tmp2 = formula((n / 2LL));
+		memo[count].value=(((2*tmp+tmp2)) *tmp2)%mod;
+		memo[count].key = n;
+		count++;
+		return memo[count-1].value;
+	}
+}
+int memoization(long long n)
+{
+	for (int i = 0; i < count; i++)
+	{
+		if (n == memo[i].key)
+		{
+			return i;
+		}
+	}
+	return 0;
+}
