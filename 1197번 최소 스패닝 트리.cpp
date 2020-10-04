5b561300f3cc25a681f3bd5355b1d06bb76a9d19
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2020-10-04 21:31:21 +0900

    Add files via upload

---
+++ 1197번 최소 스패닝 트리.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+#define SIZE 10000
+
+using namespace std;
+
+struct  EDGE {
+	int x, y, w;
+};
+
+typedef struct EDGE Edge;
+bool compare(Edge a, Edge b)
+{
+	return a.w < b.w;
+}
+
+int parent[SIZE + 1];
+vector <Edge> edges;
+long long result = 0;
+void kruskal();
+void init();
+int _parent(int x);
+bool _union(int x, int y);
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int v, e;
+	int x, y, w;
+	cin >> v >> e;
+	// edge을 입력 받을때마다 정렬 O(eloge)
+
+	for (int i = 0; i < e; i++)
+	{
+		cin >> x >> y >> w;
+		edges.push_back({x,y,w});
+	}
+
+	init();
+	sort(edges.begin(), edges.end(), compare);
+	kruskal();
+
+	cout << result;
+
+	return 0;
+}
+void kruskal()
+{
+	for (unsigned int i = 0; i < edges.size(); i++)
+	{
+		if (_union(edges[i].x, edges[i].y))
+		{
+			result += edges[i].w;
+		}
+	}
+}
+void init()
+{
+	for (int i = 0; i < SIZE + 1; i++)
+		parent[i] = i;
+}
+int _parent(int x)
+{
+	if (parent[x] == x)
+		return x;
+	else
+		return parent[x] = _parent(parent[x]);
+}
+bool _union(int x, int y)
+{
+	x = _parent(x);
+	y = _parent(y);
+	if (x == y)
+		return false;
+	else if (x < y)
+		parent[y] = x;
+	else
+		parent[x] = y;
+	return true;
+}
---
+++ 1717번 집합의 표현.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <algorithm>
+
+#define SIZE 1000000
+
+using namespace std;
+int parent[SIZE+1];
+
+void init();
+int find(int v);
+void _union(int v,int u);
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+	int a, b, c;
+	init();
+	for (int i = 0; i < m; i++)
+	{
+		cin >> a >> b >> c;
+		if (a == 0)
+			_union(b, c);
+		else if (a == 1)
+			if (find(b) == find(c))
+				cout << "YES" << '\n';
+			else
+				cout << "NO" << '\n';
+	}
+
+
+	return 0;
+}
+void init()
+{
+	for (int i = 0; i < SIZE+1; i++)
+	{
+		parent[i] = i;
+	}
+}
+int find(int v)
+{
+	if (v == parent[v])
+		return v;
+
+	return parent[v] = find(parent[v]);
+}
+void _union(int v, int u)
+{
+	v = find(v), u = find(u);
+
+	if (v == u)
+		return;
+	parent[v] = u;
+}
