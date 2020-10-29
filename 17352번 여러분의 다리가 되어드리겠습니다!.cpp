8a8577034d5270284ec50bbf2854b2c3b720e5eb
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2020-10-29 17:38:56 +0900

    Add files via upload

---
+++ 17352번 여러분의 다리가 되어드리겠습니다!.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <algorithm>
+
+#define SIZE 300000
+
+using namespace std;
+int parent[SIZE + 1];
+
+void init();
+int find(int v);
+bool _union(int v, int u);
+
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	int x,y;
+	int ans[2] = { 0 };
+	int idx = 0;
+	cin >> n;
+	init();
+	for (int i = 0; i < n-2; i++)
+	{
+		cin >> x >> y;
+		_union(x, y);
+	}
+	for (int i = 1; i < n + 1; i++)
+	{
+		if (parent[i] == i)
+			ans[idx++] = i;
+	}
+	cout << ans[0] << " " << ans[1];
+
+
+	return 0;
+}
+void init()
+{
+	for (int i = 0; i < SIZE + 1; i++)
+	{
+		parent[i] = i;
+	}
+}
+int find(int v)
+{
+	if (v == parent[v])
+		return v;
+
+	return parent[v] = find(parent[v]);
+}
+bool _union(int v, int u)
+{
+	v = find(v), u = find(u);
+
+	if (v == u)
+		return false;
+	else if(v>u)
+		parent[u] = v;
+	else
+		parent[v] = u;
+	return true;
+}
---
+++ 2381번 최대거리.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct point {
+	int x, y;
+};
+typedef struct point Point;
+
+int dist(Point* a, Point* b)
+{
+	return abs(a->x - b->x) + abs(a->y - b->y);
+}
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+
+	Point* arr = new Point[n];
+	int* ap = new int[n];
+	int* am = new int[n];
+
+	for (int i = 0; i < n; i++)
+		cin >> arr[i].x >> arr[i].y, ap[i]= arr[i].x + arr[i].y, am[i] = arr[i].x - arr[i].y;
+	
+	sort(ap, ap + n);
+	sort(am, am + n);
+
+	cout << max(ap[n-1]-ap[0],am[n-1]-am[0]);
+
+	return 0;
+}
