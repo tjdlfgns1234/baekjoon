a81083378d80074587f7342136d8adfafe2f7414
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2020-12-22 22:28:52 +0900

    Add files via upload

---
+++ 백준 11819번 The Shortest does not Mean the Simplest.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+map <unsigned long long, unsigned long long>mp;
+unsigned long long mul(unsigned long long a, unsigned long long b, unsigned long long mod);
+unsigned long long multi(unsigned long long a, unsigned long long b, unsigned long long mod);
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	unsigned long long a, b, c;
+	cin >> a >> b >> c;
+	cout << multi(a%c, b, c);
+
+	return 0;
+}
+unsigned long long multi(unsigned long long a, unsigned long long b, unsigned long long mod)
+{
+	if (b == 1)
+		return mp[1] = a % mod;
+	else if (b == 0)
+		return 1;
+	else if (mp.find(b) != mp.end())
+		return mp[b];
+	else if ((b % 2) == 1)
+		return mp[b] = mul(multi(a%mod, (b / 2) + 1, mod), multi(a % mod, (b / 2), mod),mod)% mod;
+	else
+		return mp[b] = mul(multi(a% mod, b / 2, mod), multi(a% mod, b / 2, mod),mod) % mod;
+}
+unsigned long long mul(unsigned long long a, unsigned long long b, unsigned long long mod)
+{
+	// a la russe 방식
+	unsigned long long sum = 0;
+	while (a > 0)
+	{
+		if (a & 1)
+			sum += b, sum %= mod;
+		a >>= 1, a %= mod;
+		b <<= 1, b %= mod;
+	}
+	return sum % mod;
+}
---
+++ 백준 4779번 칸토어 집합.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <algorithm>
+#define SIZE 1000000
+using namespace std;
+
+bool str[SIZE + 1] = { false };
+void cantor(int start, int end);
+void print_cantor(int start, int end);
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	int start = 1,end = 1;
+
+	while (1)
+	{	
+		start = 1, end = 1;
+		cin >> n;
+		if (cin.eof())
+			break;
+		for (int j = 0; j < n; j++)
+			end *= 3;
+		cantor(1, end);
+		print_cantor(1, end);
+		for (int i = 1; i < end+1; i++)
+			str[i] = false;
+	}
+
+	return 0;
+}
+void cantor(int start, int end)
+{
+	if (end - start + 1 <= 1)   // Base case
+		return;
+	int size = end - start + 1;
+	int low = start + size / 3;
+	int upp = low + size/3;
+	for (int i = low; i < upp; i++)
+		str[i] = true;
+	// Recursion
+	cantor(start, low-1);
+	cantor(upp, end);
+}
+void print_cantor(int start, int end)
+{
+	for (int i = start; i < end+1; i++)
+		if (str[i])
+			cout << " ";
+		else
+			cout << "-";
+	cout << "\n";
+}
