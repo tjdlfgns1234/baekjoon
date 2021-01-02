13de721496150761c9ca9950dfde67eab6cdcc66
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2021-01-02 19:04:04 +0900

    Add files via upload

---
+++ 1673번 치킨 쿠폰.cpp
@@ -0,0 +1,31 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(NULL);
+	cout.tie(NULL), cin.tie(NULL);
+
+	int n, k;
+	while (1)
+	{
+		cin >> n >> k;
+		if (cin.eof())
+			break;
+		long long voucher = 0;
+		long long ans = 0;
+		while (1)
+		{
+			ans += n;
+			voucher += n;
+			n = voucher / k;
+			voucher = voucher % k;
+			if (n == 0)
+				break;
+		}
+		cout << ans << '\n';
+	}
+
+	return 0;
+}
