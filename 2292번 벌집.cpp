6697ff73de14ff183bea633ef6f63665ad51317f
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2021-01-14 18:26:09 +0900

    Add files via upload

---
+++ 2292번 벌집.cpp
@@ -0,0 +1,28 @@
+﻿#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	int n;
+	cin >> n;
+	int s = 6;
+	int ans = 1;
+
+	while (1)
+	{
+		if (n <= 3*ans*ans - 3*ans + 1)
+			break;
+		ans++;
+	}
+	if (n == 1)
+		cout << 1;
+	else
+		cout << ans;
+
+	return 0;
+}
