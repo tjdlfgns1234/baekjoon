b49286887a8c71529438cc6f42197cfe157678f8
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2021-01-18 20:37:26 +0900

    Add files via upload

---
+++ 10816번 숫자 카드 2.cpp
@@ -0,0 +1,23 @@
+﻿#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	int tmp;
+	for (int i = 0; i < n; i++)
+		cin >> tmp, arr[i] = tmp;
+	sort(arr.begin(), arr.end());
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> tmp, cout << upper_bound(arr.begin(), arr.end(), tmp) - lower_bound(arr.begin(), arr.end(), tmp) << ' ';
+
+	return 0;
+}
