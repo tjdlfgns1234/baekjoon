6ecc4a69e5735e49f9a0410d23d9f9ccbf6f232c
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2021-01-19 17:51:08 +0900

    Add files via upload

---
+++ 1874번 스택수열.cpp
@@ -0,0 +1,44 @@
+﻿#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	stack <int>s;
+	vector <char> ans;
+	int* arr = new int[n];
+	for(int i = 0 ; i < n;i++)
+		cin >> arr[i];
+	int j = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		s.push(i);
+		ans.push_back('+');
+
+		while (!s.empty())
+		{
+			if (s.top() == arr[j])
+			{
+				s.pop();
+				ans.push_back('-');
+				j++;
+			}
+			else
+				break;
+		}
+	}
+
+	if (!s.empty())
+		cout << "NO";
+	else
+		for (auto i : ans)
+			cout << i << '\n';
+
+	return 0;
+}
---
+++ 4949번 균형잡힌 세상.cpp
@@ -0,0 +1,61 @@
+﻿#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	char s[106];
+	while (1)
+	{
+		fgets(s, 105, stdin);
+		if (s[0] == '.')
+			break;
+		bool chk = false;
+		
+		stack <char> stack;
+		int n = strlen(s);
+		for (int i = 0; i < n; i++)
+		{
+			if (s[i] == '(')
+				stack.push('(');
+			else if (s[i] == ')')
+			{
+				if (!stack.empty())
+				{
+					if (stack.top() != '(')
+						chk = true, stack.pop();
+					else if (stack.top() == '(')
+						stack.pop();
+				}
+				else
+					chk = true;
+			}
+			else if (s[i] == '[')
+				stack.push('[');
+			else if (s[i] == ']')
+			{
+				if (!stack.empty())
+				{
+					if (stack.top() != '[')
+						chk = true, stack.pop();
+					else if (stack.top() == '[')
+						stack.pop();
+				}
+				else
+					chk = true;
+			}
+		}
+		if (!stack.empty())
+			chk = true;
+		if (chk)
+			cout << "no" << '\n';
+		else
+			cout << "yes" << '\n';
+	}
+
+	return 0;
+}
