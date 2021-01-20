ec52c17e4d6fc643fa79c96c951e5d235f60a871
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2021-01-20 20:36:43 +0900

    Add files via upload

---
+++ 15829번 Hashing.cpp
@@ -0,0 +1,35 @@
+﻿#include <bits/stdc++.h>
+#define ll long long
+#define mod 1234567891
+
+using namespace std;
+
+void solve();
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	solve();
+
+	return 0;
+}
+void solve()
+{
+	string s;
+	int l;
+	cin >> l;
+	cin >> s;
+
+	ll sum = 1;
+	ll tmp;
+	ll ans = 0;
+	for (int i = 0; i < l; i++)
+	{
+		tmp = s[i] - 'a' + 1;
+		ans += (sum * tmp)%mod;
+		sum = (31 * sum) % mod;
+	}
+	cout << ans%mod;
+}
---
+++ 2775번 부녀회장이 될테야.cpp
@@ -0,0 +1,38 @@
+﻿#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+void solve();
+int arr[15][15] = { 0 };
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int t;
+	cin >> t;
+	solve();
+	while (t--)
+	{
+		int k,n;
+		cin >> k >> n;
+		cout << arr[k][n] << '\n';
+	}
+	return 0;
+}
+void solve()
+{
+	int sum = 0;
+	for (int i = 1; i < 15; i++)
+		arr[0][i] = i;
+	for (int i = 1; i < 15; i++)
+	{
+		sum = 0;
+		for (int j = 1; j < 15; j++)
+		{
+			sum += arr[i - 1][j];
+			arr[i][j] = sum;
+		}
+	}
+}
