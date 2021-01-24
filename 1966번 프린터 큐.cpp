fa93756ca65c88a8b5acfdce9d15c4f06b5b774b
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2021-01-24 19:26:38 +0900

    Add files via upload

---
+++ 1966번 프린터 큐.cpp
@@ -0,0 +1,40 @@
+﻿#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n,m;
+		cin >> n >> m;
+		int ans = 0;
+		int* arr = new int[n];
+		priority_queue <int, vector<int>> pq;
+		queue <int> q;
+		for (int i = 0; i < n; i++)
+			cin >> arr[i],pq.push(arr[i]),q.push(i);
+
+		while (!pq.empty())
+		{
+			if (pq.top() == arr[q.front()])
+				if (q.front() == m)
+				{
+					ans++;
+					break;
+				}
+				else
+					pq.pop(), q.pop(),ans++;
+			else
+				q.push(q.front()), q.pop();
+		}
+		cout << ans << '\n';
+	}
+	return 0;
+}
---
+++ 2805번 나무 자르기.cpp
@@ -0,0 +1,40 @@
+﻿#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	ll n, m;
+	cin >> n >> m;
+	ll* arr = new ll[n];
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	ll sum = 0ll;
+	ll upper= (ll)1e10, low = 0ll;
+	ll mid;
+	while (1)
+	{
+		mid = (upper + low) / 2;
+		sum = 0ll;
+		for (int i = 0; i < n; i++)
+			if(arr[i]-mid >0)
+				sum += arr[i] - mid;
+		if (upper - low <= 1)
+			break;
+		if (sum > m)
+			low = mid;
+		else if (sum == m)
+			break;
+		else
+			upper = mid;
+	}
+
+	cout << mid;
+
+	return 0;
+}
