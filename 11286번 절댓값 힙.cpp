3d523a7d1afceb94e29fab764d3964c7c62097ea
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2021-01-28 09:17:49 +0900

    Add files via upload

---
+++ 11286번 절댓값 힙.cpp
@@ -0,0 +1,47 @@
+﻿#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int t;
+	cin >> t;
+	priority_queue<int, vector<int>,greater<int>> pos;
+	priority_queue<int, vector<int>, less<int>>neg;
+	while (t--)
+	{
+		int x;
+		cin >> x;
+
+		
+		if(x==0)
+		{
+			if (pos.empty())
+				if (neg.empty())
+					cout << 0 << "\n";
+				else
+					cout << neg.top() << "\n", neg.pop();
+			else
+				if (neg.empty())
+					cout << pos.top() << "\n", pos.pop();
+				else
+					if (-neg.top() <= pos.top())
+						cout << neg.top() << "\n", neg.pop();
+					else
+						cout << pos.top() << "\n", pos.pop();
+		}
+		else
+		{
+			if (x < 0)
+				neg.push(x);
+			else
+				pos.push(x);
+		}
+	}
+
+	return 0;
+}
---
+++ 15917번 노솔브 방지문제야!!.cpp
@@ -0,0 +1,39 @@
+﻿#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int q;
+	cin >> q;
+	while (q--)
+	{
+		int n;
+		cin >> n;
+		bool chk = false;
+		if (n != 1)
+		{
+			while (1)
+			{
+				int remain = n % 2;
+				n = n / 2;
+				if (remain != 0)
+					break;
+				else if (n == 1)
+				{
+					chk = true;
+					break;
+				}
+			}
+		}
+		else
+			chk = true;
+		cout << chk << '\n';
+
+	}
+	return 0;
+}
