c141f83fe3410623af319fb9d431b7a7c355ef7b
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2021-01-29 00:27:53 +0900

    Add files via upload

---
+++ 10886번.cpp
@@ -0,0 +1,30 @@
+﻿#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	int one=0, two=0;
+
+	int tmp;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> tmp;
+		if (tmp == 1)
+			one++;
+		else
+			two++;
+	}
+	if (one < two)
+		cout << "Junhee is not cute!";
+	else
+		cout << "Junhee is cute!";
+
+	return 0;
+}
