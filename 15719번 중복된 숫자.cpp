81afc2b38e8db62b2d00f549da108356472aaccf
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2021-02-01 19:35:32 +0900

    Add files via upload

---
+++ 15719번 중복된 숫자.cpp
@@ -0,0 +1,25 @@
+﻿#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+
+int main() {
+	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	ll n;
+	cin >> n;
+	ll sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		ll tmp;
+		cin >> tmp;
+		sum += tmp;
+	}
+	sum = sum - ((n) * (n-1)) / 2;
+
+	cout << sum;
+
+	return 0;
+}
---
+++ 4592번 중복을 없애자.cpp
@@ -0,0 +1,27 @@
+﻿#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+int main() {
+	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	
+	while (1)
+	{
+		int n;
+		cin >> n;
+		if (n==0)
+			break;
+		vector <int> arr(n);
+		for (auto& i : arr)
+			cin >> i;
+		arr.erase(unique(arr.begin(), arr.end()),arr.end());
+		
+		for (auto i : arr)
+			cout << i << ' ';
+		cout << "$\n";
+	}
+	return 0;
+}
