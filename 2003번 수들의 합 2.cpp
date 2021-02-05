7d3fd4cd34df5f96974b016af2a396ec76183741
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2021-02-05 19:13:45 +0900

    Add files via upload

---
+++ 2003번 수들의 합 2.cpp
@@ -0,0 +1,79 @@
+﻿#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	/*int t;
+	cin >> t;
+	while (t--)
+	{
+
+	}*/
+
+	ll n,k;
+	cin >> n >> k;
+	vector <ll> arr(n);
+	for (auto& i : arr)
+		cin >> i;
+
+	ll left = arr[0];
+	ll right = arr[0];
+	int lidx = 0;
+	int ridx = 0;
+	ll ans = 0;
+	ll sum = left;
+	if (sum == k)
+	{
+		ans++;
+		ridx++;
+		if (ridx == n)
+		{
+			cout << ans;
+			return 0;
+		}
+		right = arr[ridx];
+		sum += right;
+	}
+	while (1)
+	{
+		if (sum == k)
+		{
+			sum -= left;
+			ans++;
+			lidx++;
+			if (lidx >= n)
+				break;
+			left = arr[lidx];
+		}
+		else if (sum < k)
+		{
+			if (ridx < n - 1)
+			{
+				ridx++;
+				right = arr[ridx];
+				sum += right;
+			}
+			else
+			{
+				break;
+			}
+		}
+		else
+		{
+			sum -= left;
+			lidx++;
+			if (lidx >= n)
+				break;
+			left = arr[lidx];
+		}
+		
+	}
+	cout << ans;
+
+	return 0;
+}
---
+++ 2018번 수들의 합 5.cpp
@@ -0,0 +1,54 @@
+﻿#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	
+	/*int t;
+	cin >> t;
+	while (t--)
+	{
+
+	}*/
+
+	int n;
+	cin >> n;
+	int idx = 1;
+
+	int left = 1;
+	int right = 1;
+	int ans = 0;
+	int sum = left;
+	while (1)
+	{
+		if (left == n)
+		{
+			ans++;
+			break;
+		}
+		else if (sum == n)
+		{
+			ans++;
+			right++;
+			sum += right;
+		}
+		else if (sum < n)
+		{
+			right++;
+			sum += right;
+		}
+		else
+		{
+			sum -= left;
+			left++;
+		}
+		
+	}
+	cout << ans;
+
+	return 0;
+}
