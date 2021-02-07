a3b22ecd29aa4063f20f65247c0904fe50571b1d
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2021-02-07 19:54:58 +0900

    Add files via upload

---
+++ 2559번 수열.cpp
@@ -0,0 +1,38 @@
+﻿#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+
+// Beware of MAX & MIN value!
+
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int n, k;
+	cin >> n >> k;
+	vector <int> arr(n);
+	for (auto& i : arr)
+		cin >> i;
+
+	int ans = -10001;
+	int sum = 0;
+	int idx = 0;
+	for (int i = 0; i < k; i++)
+		sum += arr[i];
+	while (1)
+	{
+		ans = max(ans, sum);
+
+		if (idx + k >= n)
+			break;
+		sum -= arr[idx];
+		sum += arr[idx + k];
+		idx++;
+	}
+	cout << ans;
+
+	return 0;
+}
