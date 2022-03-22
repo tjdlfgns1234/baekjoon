2e08a736def6cd1477e2388a545a8a640feebbb2
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2022-03-22 18:21:26 +0900

    Add files via upload

---
+++ 18096번 Арифметическая магия.cpp
@@ -0,0 +1,17 @@
+﻿#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define all(a) a.begin(),a.end()
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    cout << 1;
+
+    return 0;
+}
---
+++ 23968번 알고리즘 수업 - 버블 정렬 1.cpp
@@ -0,0 +1,41 @@
+﻿#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define all(a) a.begin(),a.end()
+using namespace std;
+
+void bubble_sort(vector<int>& arr, int k);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> arr(n);
+
+    for (auto& i:arr)
+        cin >> i;
+
+    bubble_sort(arr, k);
+
+    return 0;
+}
+void bubble_sort(vector<int>& arr, int k)
+{
+    int cnt = 0;
+    for (int i = arr.size(); i > 1; i--)
+        for (int j = 0; j < arr.size()-1; j++)
+            if (arr[j] > arr[j + 1]) {
+                cnt++;
+                if (cnt == k) {
+                    cout << arr[j + 1] << " " << arr[j];
+                    return;
+                }
+                else
+                    swap(arr[j], arr[j + 1]);
+            }
+    if (cnt < k)
+        cout << -1;
+}
