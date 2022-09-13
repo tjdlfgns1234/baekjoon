1e17a9e8e282e83973fde88d95d9c49d98a2ce2f
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2022-09-13 16:55:29 +0900

    Add files via upload

---
+++ 2230번 수 고르기.cpp
@@ -0,0 +1,48 @@
+﻿#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define all(a) a.begin(),a.end()
+using namespace std;
+
+void solve();
+
+int main(void)
+{
+	ios::sync_with_stdio(NULL);
+	::cout.tie(NULL), cin.tie(NULL);
+
+	solve();
+
+	return 0;
+}
+void solve() {
+	int n, m;
+	cin >> n >> m;
+
+	vector<int> arr(n);
+	for (auto& i : arr)
+		cin >> i;
+
+	sort(all(arr));
+
+	int l = 0, r = 0;
+	int sum = 0, ans = INT_MAX;
+	while (1) {
+		if (l >= n || r>= n )
+			break;
+		sum = abs(arr[l] - arr[r]);
+
+		if (sum > m) {
+			ans = min(sum, ans);
+			r++;
+		}
+		else if (sum < m)
+			l++;
+		else {
+			ans = sum;
+			break;
+		}
+	}
+
+	cout << ans;
+}
