85a044248051c7a380c05b6b3d8d2ffeddf2a5be
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2023-04-15 15:17:33 +0900

    Add files via upload

---
+++ 15663번 N과 M (9).cpp
@@ -0,0 +1,67 @@
+﻿#include <bits/stdc++.h>
+#include <unordered_map>
+#define all(a) a.begin(),a.end()
+
+// beware of min & max value;
+// 빈 곳의 개수를 세는 것도 그래프를 잘 푸는 방법중 하나!
+// /와 %연산은 배열을 나타낼때 유용하게 쓰인다.
+// 우선순위 큐에 무엇을 넣을지도 잘 생각하자.
+// 큐 쓰는 것도 좋았지만, 최적화는 잘해주자.
+// 행렬을 배열로도 쓸 생각하자
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+void solve();
+void dfs(vector<int> &arr,vector<int>  ans, int m);
+int chk[8] = { 0 };
+map<string, int> mp;
+
+int main()
+{
+    ios::sync_with_stdio(NULL);
+    std::cout.tie(NULL), cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> arr(n);
+    for (auto& i : arr) cin >> i;
+
+    sort(all(arr));
+    vector<int> ans;
+    dfs(arr,ans, m);
+}
+void dfs(vector<int>& arr, vector<int>  ans, int m){
+    if (ans.size() > m)
+        return;
+    else if (ans.size() != m) {
+        for(int i = 0; i < arr.size(); i++)
+            if (!chk[i]) {
+                ans.push_back(arr[i]);
+                chk[i] = 1;
+                dfs(arr, ans, m);
+                chk[i] = 0;
+                ans.pop_back();
+            }
+    } 
+    else if(ans.size() == m) {
+        string s = "";
+        for (auto i : ans)
+            s += to_string(i) + " ";
+        s+= '\n';
+        if (mp.count(s) == 0)
+            mp.insert({ s,1 }), cout << s;
+        else
+            return;
+    }
+}
---
+++ 2870번 수학문제.cpp
@@ -0,0 +1,77 @@
+﻿#include <bits/stdc++.h>
+#include <unordered_map>
+#define all(a) a.begin(),a.end()
+
+// beware of min & max value;
+// 빈 곳의 개수를 세는 것도 그래프를 잘 푸는 방법중 하나!
+// /와 %연산은 배열을 나타낼때 유용하게 쓰인다.
+// 우선순위 큐에 무엇을 넣을지도 잘 생각하자.
+// 큐 쓰는 것도 좋았지만, 최적화는 잘해주자.
+// 행렬을 배열로도 쓸 생각하자
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+void solve();
+int main()
+{
+    ios::sync_with_stdio(NULL);
+    std::cout.tie(NULL), cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<string> arr(n);
+    vector<string> ans;
+    for (auto& i : arr)cin >> i;
+
+    
+    string tmp = "";
+    for (int i = 0; i < n; i++) {  
+        for (int j = 0; j < arr[i].size(); j++) {
+            if (arr[i][j] >= '1' && arr[i][j] <= '9') {
+                if (tmp.size() == 1)
+                    if (tmp[0] == '0')
+                        tmp.clear();
+                tmp += arr[i][j];
+            }
+            else if (arr[i][j] == '0') {
+                if (tmp.size() == 1) {
+                    if (tmp[0] != '0')
+                        tmp += arr[i][j];
+                }
+                else
+                    tmp += arr[i][j];
+            }
+            else {
+                if (tmp.size() != 0)
+                    ans.push_back(tmp), tmp.clear();
+            }
+        }
+        if (tmp.size() != 0)
+            ans.push_back(tmp), tmp.clear();
+    }
+
+    for (int i = 0; i < ans.size(); i++)
+        for (int j = i+1; j < ans.size(); j++)
+            if (ans[i].size() == ans[j].size()) {
+                if (ans[i] > ans[j])
+                    swap(ans[i], ans[j]);
+            }
+            else  if (ans[i].size() > ans[j].size()) 
+                swap(ans[i], ans[j]);
+            
+
+
+    for (auto i : ans)
+        cout << i << '\n';
+}
