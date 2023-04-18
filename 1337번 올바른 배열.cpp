8269f823f637d5384c6b3075b6b7fc52f615b0bf
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2023-04-19 00:05:48 +0900

    Add files via upload

---
+++ 1337번 올바른 배열.cpp
@@ -0,0 +1,51 @@
+﻿#include <bits/stdc++.h>
+#include <unordered_map>
+#define all(a) a.begin(),a.end()
+
+// beware of min & max value;
+// 빈 곳의 개수를 세는 것도 그래프를 잘 푸는 방법중 하나!
+// /와 %연산은 배열을 나타낼때 유용하게 쓰인다.
+// 우선순위 큐에 무엇을 넣을지도 잘 생각하자.
+// 큐 쓰는 것도 좋았지만, 최적화는 잘해주자.
+// 행렬을 배열로도 쓸 생각하자
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+void solve();
+
+int main()
+{
+    ios::sync_with_stdio(NULL);
+    std::cout.tie(NULL), cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (auto& i : arr)cin >> i;
+
+    sort(all(arr));
+
+    int ans = 5;
+    for (int i = 0; i < n; i++) {
+        int x = arr[i], cnt = 1;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] - x <= 4)
+                cnt++;
+            else
+                break;
+        }
+        ans = min(ans, 5 - cnt);
+    }
+    cout << ans;
+}
