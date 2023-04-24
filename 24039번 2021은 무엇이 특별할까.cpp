dd180467d68edebbb23bfd0305403f6832b07ff4
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2023-04-24 19:40:27 +0900

    Add files via upload

---
+++ 24039번 2021은 무엇이 특별할까.cpp
@@ -0,0 +1,61 @@
+﻿#include <bits/stdc++.h>
+#include <unordered_map>
+#define all(a) a.begin(),a.end()
+
+// beware of min & max value;
+// 빈 곳의 개수를 세는 것도 그래프를 잘 푸는 방법중 하나!
+// /와 %연산은 배열을 나타낼때 유용하게 쓰인다.
+// 우선순위 큐에 무엇을 넣을지도 잘 생각하자.
+// 큐 쓰는 것도 좋았지만, 최적화는 잘해주자.
+// 행렬을 배열로도 쓸 생각하자
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+void solve();
+bool isprime(int n);
+
+int main()
+{
+    ios::sync_with_stdio(NULL);
+    std::cout.tie(NULL), cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+void solve() {
+    int n;
+    cin >> n;
+
+
+    vector<int> arr;
+    arr.push_back(2);
+    for (int i = 3; i <= 10000; i++)
+        if (isprime(i))
+            arr.push_back(i);
+
+    vector<int> ans;
+    for (int i = 1; i < arr.size(); i++)
+        ans.push_back(arr[i]*arr[i-1]);
+ 
+
+    for(auto i : ans)
+        if (i > n) {
+            cout << i;
+            return;
+        }
+
+}     
+bool isprime(int n) {
+    if (n % 2 == 0)
+        return false;
+    for (int i = 3; i * i <= n; i += 2)
+        if (n % i == 0)
+            return false;
+    return true;
+}
