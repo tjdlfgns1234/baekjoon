1c02342ab2c075f5a5b7ad255cb0a0ac82961c4c
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2023-06-10 12:27:59 +0900

    Add files via upload

---
+++ 25083 새싹.cpp
@@ -0,0 +1,43 @@
+﻿#include <bits/stdc++.h>
+#include <unordered_map>
+#define all(a) a.begin(),a.end()
+#define mod 1000000007
+
+// beware of min & max value;
+// 빈 곳의 개수를 세는 것도 그래프를 잘 푸는 방법중 하나!
+// /와 %연산은 배열을 나타낼때 유용하게 쓰인다.
+// 배운거 내에서만 쓸 생각을 하자. 
+// 평소 연습했던 대로!
+// 행렬을 배열로도 쓸 생각하자
+// 급하기 풀지 말자
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+void solve();
+
+int main()
+{
+    ios::sync_with_stdio(NULL);
+    std::cout.tie(NULL), cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+void solve() {
+   
+    cout << "         ,r\'\"7\n";
+    cout << "r`-_   ,'  ,/" << '\n';
+    cout << " \\. \". L_r\'\n";
+    cout << "   `~\\/\n";
+    cout << "      |\n";
+    cout << "      |\n";
+
+
+
+}
